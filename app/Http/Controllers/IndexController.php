16220d03f1c6fbc23166d460b26e6520c963ab4c
Mr. Long <josephminhlong@gmail.com>
2023-02-26 16:37:59 +0700

    thumbs image app.js

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\TablePhoto;
 use App\Models\TableStatic;
 use App\Models\TableProduct;
 use App\Models\TableProductList;
 use App\Models\TablePost;
+use File;
 
 class IndexController extends Controller
 {
@@ -15,49 +17,43 @@ class IndexController extends Controller
     {
         // slide,gioithieu,pronb,banner_sanpham,splistnb,bannerQC,album,postnb
         $slide = TablePhoto::where('type', 'slideshow')->get();
-        $gioithieu = TableStatic::where('type', 'gioi-thieu')->get();
+        $gioithieu = TableStatic::where('type', 'gioi-thieu')->first();
+        $gioithieu_slide = TablePhoto::where('type', 'gioithieu-slide')->get();
         $banner_sanpham = TablePhoto::where('type', 'banner-sanpham')->get();
         $bannerQC = TablePhoto::where('type', 'banner-quangcao')->get();
         $album = TablePhoto::where('type', 'album')->get();
 
         $pronb = TableProduct::whereJsonContains('status', 'noibat')->get();
-        $splistnb = TableProductList::whereJsonContains('status', 'noibat')->get();
+        $splistnb = DB::table('table_product_lists')
+        ->whereJsonContains('status', "noibat,hienthi")
+        ->get();
+
+        // dd($gioithieu_slide);
         $postnb = TablePost::whereJsonContains('status', 'noibat')->get();
 
 
 
         return view('template.index.index',compact([
             'slide',
+            'splistnb',
+            'gioithieu',
+            'gioithieu_slide',
         ]));
     }
 
-    public function create()
-    {
-        //
-    }
 
-    public function store(Request $request)
+    public function thumbs_img(Request $request)
     {
-        //
-    }
+        $width =  $request->get('width');
+        $height =  $request->get('height');
 
-    public function show($id)
-    {
-        //
-    }
+        $size = $width.'x'.$height;
+        die($size);
+        $path = public_path().'/thumbs/'.$size ;
+        if(!file_exists($path)){
+            File::makeDirectory($path);
+        }
 
-    public function edit($id)
-    {
-        //
-    }
 
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    public function destroy($id)
-    {
-        //
     }
 }
