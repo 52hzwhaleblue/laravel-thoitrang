080420d6510899cd4cab8abb2ab045b23f130654
Tryhard <chuong.2xx1.do@gmail.com>
2023-04-19 21:02:50 +0700

    Use DI

--- app/Jobs/InsertReviewJob.php
+++ app/Jobs/InsertReviewJob.php
@@ -8,7 +8,6 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 
 class InsertReviewJob implements ShouldQueue
 {
@@ -35,14 +34,18 @@ class InsertReviewJob implements ShouldQueue
     }
     
     public function handle()
-    {
-        $products = DB::table('table_order_details')
+    {   
+        $db = new DB();
+
+        $query_review = $db::table('table_reviews');
+
+        $products = $db::table('table_order_details')
         ->where('order_id', $this->orderId)
         ->select('id', 'product_id')
         ->get();
 
         foreach ($products as $item) {
-            DB::table('table_reviews')->insert([
+            $query_review->insert([
                 'user_id' => $this->userId,
                 'order_id' => $this->orderId,
                 'product_id' => $item->id,
