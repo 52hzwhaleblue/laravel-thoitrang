6934b326ffead1c4367e8241918be80369be58f0
Mr. Long <josephminhlong@gmail.com>
2023-05-15 23:54:40 +0700

    long update thongke lan 02

--- app/Models/TableOrder.php
+++ app/Models/TableOrder.php
@@ -48,12 +48,12 @@ class TableOrder extends Model
         'updated_at' => 'datetime:Y-m-d H:i:s',
     ];
 
-//    public function getCreatedAtAttribute($value)
-//    {
-//        return Carbon::createFromFormat('Y-m-d\TH:i:s.u\Z', $value, 'UTC')
-//            ->setTimezone('Asia/Ho_Chi_Minh')
-//            ->toDateTimeString();
-//    }
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::createFromFormat('Y-m-d\TH:i:s.u\Z', $value, 'UTC')
+            ->setTimezone('Asia/Ho_Chi_Minh')
+            ->toDateTimeString();
+    }
 
     public function users()
     {
